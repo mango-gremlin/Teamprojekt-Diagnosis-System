71e477438865108f2c338c7baae304010dabe9cb
mango-gremlin <mara.cockburn@student.uni-tuebingen.de>
2023-04-25 19:26:55 +0200

    added ml and gui examples from wolfgang fuhl

---
+++ MLExample/ConvertDATASETtoMATfile.m
@@ -0,0 +1,111 @@
+function ConvertDATASETtoMATfile()
+
+
+    diseases={}; %Cell array for the diseases since we have strings here; with [] you init a normal array with numerical values
+    diseases_index1=0;  %For the target class, the diseas here, we only need a one dimensional array since every entry will have only one diseas
+
+    symptoms=[]; %Here we will use a numerical array and assigne a 1 to the symptoms the patient has and otherwiese zero
+    symptoms_index1=0;
+
+
+    all_symptoms={}; %Cell array for the symptoms since we need to store the text first so we can create the symptoms table in a second step
+    all_symptoms_index1=0;  % Since each symptom
+
+
+    % The main problem with this data set is, that the symptoms are given
+    % in an unordered way and not all symptoms have a value per entry.
+    % Therefore, we have to collect all possible symptoms first and make a
+    % structured array for the machine learning models.
+
+
+
+    current_file=fopen('dataset.csv'); %open the file
+
+    line=fgetl(current_file); %Skip first line since it contains only the headers
+
+    while ~feof(current_file)
+
+        line=fgetl(current_file); %get current line in the file
+
+        current_data=strsplit(line,','); %split the read line on all ','
+        % If there is a ',,', ',,,' or more ',' in succession no new index in current_data
+        % will be generated, those values will be ignored from the strsplit function
+
+        diseases_index1=diseases_index1+1; %increase index
+        diseases{diseases_index1,1}=deblank(current_data{1,1}); %Remove trailing whitespace and null characters (deblank). Add the string to the table
+
+        for i=2:size(current_data,2)
+            if ~isempty(deblank(current_data{1,i}))
+                all_symptoms_index1=all_symptoms_index1+1; %increase index
+                all_symptoms{all_symptoms_index1,1}=deblank(current_data{1,i}); %Remove trailing whitespace and null characters (deblank). Add the string to the table
+            end
+        end
+
+
+    end
+
+
+    size(diseases)  %if you do not set the ; at the end MATLAB will plot the result in the console (Good for debugging)
+    % you should get the size of all entries in the .csv without the first
+    % line.
+
+    size(all_symptoms)  %size of all symptoms
+
+    unique_symptoms=unique(all_symptoms);  %removes all dublicates from the array of symptoms
+
+    size(all_symptoms) %size of all unique symptoms
+
+    fclose(current_file); %close the file
+
+
+
+    symptoms=zeros(size(diseases,1),size(unique_symptoms,1)); %Now we can preallocate our array of features (symptoms)
+
+    %The first size we need is the amount of entries or records in our
+    %table (size(diseases,1)). The second size we need is the amount of
+    %features. In our case this is the amount of unique symptoms (size(all_symptoms,1))
+
+
+
+    current_file=fopen('dataset.csv'); %open the file again to reset the data pointer to the start of the file
+    %alternatively, we could reset the "current_file" pointer but to keep
+    %it simple and understandable, we just open the file again after we
+    %have closed it.
+
+    line=fgetl(current_file); %Skip first line since it contains only the headers
+
+    while ~feof(current_file)
+
+        line=fgetl(current_file); %get current line in the file
+
+        current_data=strsplit(line,','); %split the read line on all ','
+        % If there is a ',,', ',,,' or more ',' in succession no new index in current_data
+        % will be generated, those values will be ignored from the strsplit function
+
+        symptoms_index1=symptoms_index1+1; %for each line in the file (each entry) we increase the table index for our symptoms
+
+        for i=2:size(current_data,2)
+
+            feature_index = find(strcmp(unique_symptoms,current_data{1,i})); % Since we have a cell array with strings, and we want to compare the strings to get our feature index we use the strcmp (String Compare) function.
+            % For numerical values you could use all_symptoms==current_data(1,i)
+
+            symptoms(symptoms_index1,feature_index)=1; % Based on the feature index, we set the feature (Symptom) to 1 since we preallocated the array with 0.
+
+        end
+
+
+    end
+
+    fclose(current_file); %close the file
+
+    size(diseases) % Size of our target array 4920 entries and one label (The diseas).
+    size(symptoms) % Size of our feature array 4920 entries and 132 features (The symptoms).
+
+    save('dataset.mat','diseases','symptoms','unique_symptoms'); %Here we save our tables with the diseas, our target class, and the symptoms. Additionally we save the unique features, which we will need in our GUI example.
+
+
+end
+
+
+
+
---
+++ MLExample/TrainMLmodel.m
@@ -0,0 +1,59 @@
+function TrainMLmodel()
+
+
+
+    load('dataset.mat'); %Load our dataset with diseas and symptoms
+
+    rng(1); % Since TreeBagger and the cvpartition function needs some random values, we set the random generator initialization to 1 so 
+    %when you start the script, you will get the same results as I do.
+
+    %Split the data to training and testing data
+    SplitSize = 0.20;  %Gives you a 80% training data and 20% testing data split
+    cv = cvpartition(size(diseases,1),'HoldOut',SplitSize); % This computes random indexes for the train and test set based on our amount of entries
+
+    FeaturesTrain = symptoms(cv.training,:); % Select the training data from the symptoms table as our features
+    TargetTrain = diseases(cv.training,:); % Select the training data from the diseases table as our target
+    FeaturesTest = symptoms(cv.test,:); % Select the testing data from the symptoms table as our features
+    TargetTest = diseases(cv.test,:); % Select the testing data from the diseases table as our target
+
+    MLmodel = TreeBagger(50,FeaturesTrain,TargetTrain); %Train an ensamble of bagged decition trees
+
+%     MLmodel = fitcnet(FeaturesTrain,TargetTrain,'LayerSizes',20); % Train neural network
+
+    [TargetTestPredicted TargetTestScore] = predict(MLmodel,FeaturesTest); % Use our trained model on the test set features
+%     TargetTestScore; %This is the probability of the class based on the model
+    
+
+    CnfusionMatrix = confusionmat(TargetTest,TargetTestPredicted);%Compute the confusion matrix (What have we done right and what have we done wrong)
+    confusionchart(CnfusionMatrix,unique(TargetTest),'RowSummary','row-normalized'); %Show the confusion matrix with class labels
+%     confusionchart(CnfusionMatrix,'RowSummary','row-normalized'); %Show the confusion matrix without class labels
+    %The confusion matrix has 41x41 values.
+    % You can check the sizes with:
+    %    size(TargetTest) should give you 984  1
+    %    size(unique(TargetTest)) should give you 41  1
+
+    %Accuracy example
+    %Is defined as (True Positive+True Negative) / (True Positive+True Negative + False Positive + False Negative)
+    % In other words this means, (What was correct) / (Dataset size)
+
+    our_accuracy=0.0;
+    for i=1:size(TargetTest,1) %Go over each element in the testing set
+
+        if strcmp(TargetTest{i,1},TargetTestPredicted{i,1}) %If they are equal we increase our "What was correct" part
+            our_accuracy=our_accuracy+1;
+        else
+            TargetTest{i,1}
+            TargetTestPredicted{i,1}
+        end
+        %If the array is numerical you can use TargetTest(i,1)==TargetTestPredicted(i,1)
+
+    end
+
+    our_accuracy=our_accuracy/double(size(TargetTest,1)); % Here we divide our "What was correct" with "Dataset size", the result is the accuracy
+
+    our_accuracy % Show the result in the console
+
+    save('ourmodel.mat','MLmodel','unique_symptoms');  %Here we save our model for the GUI application as well as the unique symptoms
+
+end
+
