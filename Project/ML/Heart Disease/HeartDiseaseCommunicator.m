172cb3d59e8b26fb0932b26c6a01f6eaefd432fd
Mara Cockburn <mara.cockburn@student.uni-tuebingen.de>
2023-06-21 17:12:47 +0200

    added a check for if one of the two inputs is empty

--- Project/ML/Heart Disease/HeartDiseaseCommunicator.m
+++ Project/ML/Heart Disease/HeartDiseaseCommunicator.m
@@ -8,31 +8,76 @@ classdef HeartDiseaseCommunicator
     
     methods(Static)
         
-        function outputArg = returnPrediction(input)
+        function outputArg = returnPrediction(input_joint, input_tidy)
             %RETURNPREDICTION loads the appropriate model(s) and runs a
             %prediction, outputs the prediction as a string
             
-            % NOTE: model does not exist yet, change this later
-            load(['Project' filesep 'ML' filesep 'Heart Disease' filesep 'jointHeartBaggedTreeModel.mat']);
-            model_jointHeart = jointHeartBaggedTreeModel;
-            prediction_jointHeart = model_jointHeart.predictFcn(input);
+            % check if joint_input is empty
+            rows_j = height(input_joint);
 
-            load(['Project' filesep 'ML' filesep 'Heart Disease' filesep 'tidyHeartBaggedTreeModel.mat']);
-            model_tidyHeart = tidyHeartBaggedTreeModel;
-            prediction_tidyHeart = model_tidyHeart.predictFcn(input);
+            isnotempty = false;
 
-            %compare and get the mean 
-            
-            if prediction_jointHeart(1) == prediction_tidyHeart(1)
-                if prediction_jointHeart(1) == 1
-                    outputArg = "heart disease"
-                else
-                    outputArg = "no heart disease"
+            for row = 1:rows_j
+                for col = 1:14
+                    if input_joint{row,col} ~= 0
+                            isnotempty=true;
+                    end
                 end
-            else
-                outputArg = "result inconclusive"
             end
 
+            %if it isnt empty, load model and predict for input
+            if isnotempty
+                load(['Project' filesep 'ML' filesep 'Heart Disease' filesep 'jointHeartBaggedTreeModel.mat']);
+                model_jointHeart = jointHeartBaggedTreeModel;
+                prediction_jointHeart = model_jointHeart.predictFcn(input_joint);
+            end
+
+            % check if tidy_input is empty
+            rows_j = height(input_tidy);
+
+            isnotempty_tidy = false;
+
+            for row = 1:rows_j
+                for col = 1:14
+                    if input_tidy{row,col} ~= 0
+                            isnotempty_tidy=true;
+                    end
+                end
+            end
+
+            if isnotempty_tidy
+                load(['Project' filesep 'ML' filesep 'Heart Disease' filesep 'tidyHeartBaggedTreeModel.mat']);
+                model_tidyHeart = tidyHeartBaggedTreeModel;
+                prediction_tidyHeart = model_tidyHeart.predictFcn(input_tidy);
+            end
+          
+            %compare and get a consensus result
+            if isnotempty & isnotempty_tidy
+                %if there was input for both datasets
+                if prediction_jointHeart(1) == prediction_tidyHeart(1)
+                    if prediction_jointHeart(1) == 1
+                        outputArg = "heart disease";
+                    else
+                        outputArg = "no heart disease";
+                    end
+                else
+                    outputArg = "result inconclusive";
+                end
+            else if isnotempty
+                    %if there was input only for joint
+                    if prediction_jointHeart(1) == 1
+                        outputArg = "heart disease";
+                    else 
+                        outputArg = "no heart disease";
+            end
+            else if isnotempty_tidy
+                    %if there was input only for tidy
+                    if prediction_tidyHeart(1) == 1
+                        outputArg = "heart disease";
+                    else 
+                        outputArg = "no heart disease";
+            end
+            end
         end
     end
 end
