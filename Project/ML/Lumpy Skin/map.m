0545f0826b8a66196527eb89df449b9b3d3fdb3a
VanCuchi <77912134+VanCuchi@users.noreply.github.com>
2023-07-20 11:58:24 -0500

    #Adjusted map to fit eurasia and africa (from which data comes from) #Added green and blue coloring

--- Project/ML/Lumpy Skin/map.m
+++ Project/ML/Lumpy Skin/map.m
@@ -1,7 +1,12 @@
 function [lat, lon] = map()
-    geoaxes
+    gx = geoaxes;
+    latSouth = -38;
+    lonSouth = 20;
+    latNorth = 75;
+    lonNorth = 150;
+    geoplot(gx,[latSouth latNorth],[lonSouth lonNorth],'g-*')
+    geobasemap(gx,'bluegreen')
+
     [lat, lon] = ginput(1);
     return
 end
-
-
